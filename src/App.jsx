e7162d83aa178868ed3a917d71e3c2fc665fdb26
Zahra Alvandi <zalvandi39@gmail.com>
2026-02-09 20:27:38 +0330

    Header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Components/Header/Header'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  )
+}
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <header className='flex justify-between items-center h-20'>
+            <div className='flex'>
+                <a href="#" className='text-black px-5'>خانه</a>
+                <a href="#" className='text-black px-5'>کامپوننت ها</a>
+                <a href="#" className='text-black px-5'>مقالات</a>
+                <a href="#" className='text-black px-5'>درباره ما</a>
+            </div>
+            <div>
+                <button className='ml-5'>خرید اشتراک</button>
+                <button>ورود یا ثبت نام</button>
+            </div>
+        </header>
+    </div>
+  )
+}
