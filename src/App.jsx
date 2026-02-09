432b39d3d7394e1ac4d02d52d8a7747e6fa31110
Zahra Alvandi <zalvandi39@gmail.com>
2026-02-09 21:42:33 +0330

    header  p2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 
 export default function App() {
   return (
-    <div>
+    <div className='container'>
       <Header></Header>
     </div>
   )
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
     <div>
-        <header className='flex justify-between items-center h-20'>
-            <div className='flex'>
-                <a href="#" className='text-black px-5'>خانه</a>
-                <a href="#" className='text-black px-5'>کامپوننت ها</a>
-                <a href="#" className='text-black px-5'>مقالات</a>
-                <a href="#" className='text-black px-5'>درباره ما</a>
-            </div>
-            <div>
-                <button className='px-[6] pt-4'>خرید اشتراک</button>
-                <button className='px-[6] pt-4'>ورود یا ثبت نام</button>
-            </div>
-        </header>
+      <header className="flex justify-around items-center h-20">
+        <div className="flex">
+          <a href="#" className="text-black px-5">
+            خانه
+          </a>
+          <a href="#" className="text-black px-5">
+            کامپوننت ها
+          </a>
+          <a href="#" className="text-black px-5">
+            مقالات
+          </a>
+          <a href="#" className="text-black px-5">
+            درباره ما
+          </a>
+        </div>
+        <div className="flex gap-4">
+          <button className="mx-[6] p-1 rounded-sm cursor-pointer transition flex justify-center items-center gap-2 hover:opacity-55 bg-black text-white">
+            خرید اشتراک
+          </button>
+          <button className="mx-[6] cursor-pointer transition flex justify-center items-center gap-2 hover:opacity-[0.85]">
+            ورود یا ثبت نام
+          </button>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
