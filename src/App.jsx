a9551de0edef3cc33c3af24b4d17d4e2dfaf1fe6
Zahra Alvandi <zalvandi39@gmail.com>
2026-02-24 13:38:48 +0330

    key prop

--- src/App.jsx
+++ src/App.jsx
@@ -8,48 +8,56 @@ import Button from "./Components/Button/Button";
 export default function App() {
   const articles = [
     {
+      id: 1,
       title: "ساخت اولین پروژه رزومه ای با پایتون: قدم به قدم تا موفقیت",
       author: "سعیدی راد",
       img: "/images/clay-banks-cReV-cbE2L4-unsplash.jpg",
       price: 0,
     },
     {
+      id: 2,
       title: "ساخت اولین پروژه رزومه ای با React : قدم به قدم تا موفقیت",
       author: "",
       img: "/images/snowy.jpg",
       price: 0,
     },
     {
+      id: 3,
       title: "توسعه کامپوننت های اصولی برای React",
       author: "",
       img: "/images/clay-banks-cReV-cbE2L4-unsplash.jpg",
       price: 12_000,
     },
     {
+      id: 4,
       title: "5 نکته مهم و تجربی برای React",
       author: "عبادی",
       img: "/images/snowy.jpg",
       price: 25_000,
     },
     {
+      id: 5,
       title: "برنامه نویسی در عصر هوش مصنوعی ",
       author: "خندقی",
       img: "/images/clay-banks-cReV-cbE2L4-unsplash.jpg",
       price: 32_000,
     },
     {
+      id: 6,
       title: "پرسونال برندینگ برای برنامه نویسان",
       author: "سعیدی راد",
       img: "/images/snowy.jpg",
       price: 0,
     },
     {
+      id: 7,
       title: "ساخت وبلاگ ساده با جاوا اسکریپت",
       author: "سعیدی راد",
       img: "/images/clay-banks-cReV-cbE2L4-unsplash.jpg",
       price: 0,
     },
     {
+      id: 8,
       title: "شبکه سازی حرفه ای برای برنامه نویسان",
       author: "",
       img: "/images/snowy.jpg",
@@ -69,14 +77,9 @@ export default function App() {
           <h1 className="text-3xl font-bold">مقالات اخیر</h1>
         </div>
         <div className="container mx-auto grid grid-cols-4 items-center justify-center gap-8 mt-10 mb-52">
-          <Article {...articles[0]}></Article>
-          <Article {...articles[1]}></Article>
-          <Article {...articles[2]}></Article>
-          <Article {...articles[3]}></Article>
-          <Article {...articles[4]}></Article>
-          <Article {...articles[5]}></Article>
-          <Article {...articles[6]}></Article>
-          <Article {...articles[7]}></Article>
+          {articles.map((article) => (
+            <Article key={article.id} {...article}></Article>
+          ))}
         </div>
 
         <Footer></Footer>
